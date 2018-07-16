85d5aab11528ed94e1077724b08bd24a5e7488d5
pushmyandy <1805311490@qq.com>
2018-07-16 19:30:09 +0800

    no message

--- Travel/src/router/index.js
+++ Travel/src/router/index.js
@@ -1,15 +1,21 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+import Home from '../pages/home/Home'
+import List from '../pages/list/List'
 
-Vue.use(Router)
+Vue.use(Router);
 
 export default new Router({
+  mode: 'history',
   routes: [
     {
       path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+      name: 'Home',
+      component: Home
+    },  {
+      path: '/list',
+      name: 'List',
+      component: List
     }
   ]
 })
